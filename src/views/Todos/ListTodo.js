33474a68538e39ea851cfb08336d1d52c914a16d
Nguyen Trong Quyet <super.quyet69@gmail.com>
2023-02-02 16:40:13 +0700

    Update ListTodo.js

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -23,8 +23,33 @@ class ListTodo extends React.Component {
         toast.success("xoa thanh cong" )
     }
     handleedittodo=(todo)=>{
+        let{edittodo,listTodo}=this.state;
+        let isemptyojb=Object.keys(edittodo).length === 0;
+        if(isemptyojb===false && edittodo.id === todo.id){
+            let listodocoppy =[...listTodo];
+            //Find index of specific object using findIndex method.    
+           let objIndex = listodocoppy.findIndex((item => item.id === todo.id));
+            //Update object's name property.
+            listodocoppy[objIndex].title = edittodo.title;
+            this.setState({
+                listTodo: listodocoppy,
+                edittodo:{}
+                
+            })
+            toast.success("update thanh cong")
+            return;
+        }
         this.setState({
             edittodo:todo
+        })
+        
+        
+    }
+    handleedittodochange=(event)=>{
+        let edittodocoppy = {...this.state.edittodo};
+        edittodocoppy.title=event.target.value;
+        this.setState({
+            edittodo:edittodocoppy
         })
     }
     render() {
@@ -41,18 +66,25 @@ class ListTodo extends React.Component {
                     {listTodo && listTodo.length>0 && listTodo.map((item,index) =>{
                         return(
                             <div className="todo-child" key={item.id}>
-                                {isemptyojb==true?
+                                {isemptyojb===true?
                                 <span> {index +1 }-{item.title}</span>
                                 :
                                 <>
-                                <span>{index +1 }-<input value={edittodo.title}/> </span>
+                                {edittodo.id === item.id ? 
+                                <span>{index +1 }-<input value={edittodo.title}
+                                onChange={(event)=>this.handleedittodochange(event)}
+                                /> </span>
+                                :
+                                <span>{index +1 }-{item.title} </span>
+                                }   
                                 </>
                                 
                             }
                             
                             <button className="edit"
                             onClick={()=>this.handleedittodo(item)}
-                            >Edit</button>
+                            >{isemptyojb===false && edittodo.id === item.id? 'save' :'edit'  }
+                            </button>
                             <button className="delete"
                             onClick={()=>this.handledeletetodo(item)}
                             >Delete</button>
